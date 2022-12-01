38f6f688e4ea141d8bcd23ba8c7c60f48e82119d
LouisYana <louisyanagisawawork@gmail.com>
2022-12-01 10:19:05 +1000

    a

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+	return (
+		<Router>
+			<div className='App'>
+				<Navbar />
+				<div className='content'>
+					<Switch>
+						<Route exact path='/'>
+							<Home />
+						</Route>
+						<Route path='/create'>
+							<Create></Create>
+						</Route>
+						<Route path='/blogs/:id'>
+							<BlogDetails />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
+}
+
+export default App;
